8e3712496200d802dbc7dab61bb1581361cd7659
walid barakat <myassistantprogram@gmail.com>
2021-01-26 09:00:36 +0200

    modify

--- Config/config.php
+++ Config/config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
-    'db_host' => 'sql313.byetcluster.com',
-    'db_user' => 'eb2a_27733106',
-    'db_pass' => 'He01002727562',
+    'db_host' => 'localhost',
+    'db_user' => 'root',
+    'db_pass' => '',
     'db_name' => 'eb2a_27733106_rateus'
 );
